20a19e7080b6af7808a2ade8bd65093aa2f35087
Devaki465 <bdevaki465@gmail.com>
2026-07-13 11:14:33 +0530

    second commit

---
+++ specialtypes.ts
@@ -0,0 +1,11 @@
+let stname: any="B.Devaki";
+console.log(`${stname}`);
+stname=123456;
+console.log(stname);
+let address: unknown="chinnabadam";
+console.log(address);
+console.log(typeof address);
+function addnumbers(a: number,b: number):void{
+    console.log(`${a+b}`);
+}
+addnumbers(6,7);git 
