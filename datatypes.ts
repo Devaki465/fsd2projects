b5d7d494dcfbba5573224b85c3ceeb6a014754eb
Devaki465 <bdevaki465@gmail.com>
2026-07-13 11:31:25 +0530

    Local version changes before pushing

--- datatypes.ts
+++ datatypes.ts
@@ -1,6 +1,6 @@
 let username : string ="B.Devaki"
 let rollno: number = 10;
 let present : boolean=true;
-console.log('Student name=${username}');
-console.log('Roll no:${rollno}');
-console.log('Attendence :${present}');
+console.log(`Student name=${username}`);
+console.log(`Roll no:${rollno}`);
+console.log(`Attendence :${present}`);
