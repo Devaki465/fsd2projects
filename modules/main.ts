a8d931d677358bda8739c914744b33896697f7c3
Devaki465 <bdevaki465@gmail.com>
2026-09-21 11:35:33 +0530

    Organize TypeScript files into weekly folders

--- modules/main.ts
+++
@@ -1,4 +0,0 @@
-import { add, multiply } from "./Math.js";
-
-console.log(add(10, 20));
-console.log(multiply(10, 20));
---
+++ week4/modules/main.ts
@@ -0,0 +1,4 @@
+import { add as sum, multiply } from "./Math.js";
+
+console.log(sum(10, 20));
+console.log(multiply(10, 20));
